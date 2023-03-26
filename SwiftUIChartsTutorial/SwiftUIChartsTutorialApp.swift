d5752dfefbc9fc30c9c3b8fcd4c239478efc87c9
Yasin Erdemli <yasinerdemli35@icloud.com>
2023-03-26 21:33:23 +0300

    Initial Commit

---
+++ SwiftUIChartsTutorial/SwiftUIChartsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChartsTutorialApp.swift
+//  SwiftUIChartsTutorial
+//
+//  Created by Yasin Erdemli on 26.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChartsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
